a33ea9ceafc8cf66fe88837dfde75fd9309a7f2a
Mizuki <mbaba@kanolab.net>
2023-07-14 17:50:19 +0900

    [fix] background transparency

--- dotfiles/nvim/lua/colorscheme.lua
+++ dotfiles/nvim/lua/colorscheme.lua
@@ -1,6 +1,11 @@
 vim.cmd [[
 try
   colorscheme iceberg
+    highlight Normal ctermbg=NONE guibg=NONE
+    highlight NonText ctermbg=NONE guibg=NONE
+    highlight LineNr ctermbg=NONE guibg=NONE
+    highlight Folded ctermbg=NONE guibg=NONE
+    highlight EndOfBuffer ctermbg=NONE guibg=NONEs
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
--- dotfiles/wezterm/wezterm.lua
+++ dotfiles/wezterm/wezterm.lua
@@ -6,18 +6,27 @@ if wezterm.config_builder then
   config = wezterm.config_builder()
 end
 
+-- 背景透過
+config.window_background_opacity = 0.80
+
+-- 最初からフルスクリーンで起動
+-- local mux = wezterm.mux
+-- wezterm.on("gui-startup", function(cmd)
+--     local tab, pane, window = mux.spawn_window(cmd or {})
+--     window:gui_window():toggle_fullscreen()
+-- end)
 
 config.font_size = 11
-config.font = wezterm.font("TerminessTTF Nerd Font", {weight="Regular", stretch="Normal", italic=false}) -- 自分の好きなフォントいれる
+config.font = wezterm.font("TerminessTTF Nerd Font", { weight = "Regular", stretch = "Normal", italic = false }) -- 自分の好きなフォントいれる
 -- wezは日本人じゃないのでこれがないとIME動かない
 config.use_ime = true
-config.color_scheme = "iceberg-dark"                 -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
+config.color_scheme =
+"iceberg-dark"                       -- 自分の好きなテーマ探す https://wezfurlong.org/wezterm/colorschemes/index.html
 config.line_height = 1.65
 
 if mac then
-  config.font_size = 14
+  config.font_size = 16
+  config.font = wezterm.font("FirgeNerd Console", { weight = "Regular", stretch = "Normal", italic = false }) -- 自分の好きなフォントいれる
 end
 
 return config
-
-
