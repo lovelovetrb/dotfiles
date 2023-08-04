75f974a2226e41cafb35e7ef71ec03387b6060df
Mizuki <mbaba@kanolab.net>
2023-08-04 15:04:00 +0900

    [fix] show error command for nvim

--- dotfiles/nvim/lua/custom/mappings.lua
+++ dotfiles/nvim/lua/custom/mappings.lua
@@ -1,24 +1,27 @@
 local M = {}
- M.general = {
-  i ={
+M.general = {
+  i = {
 
     ["jj"] = { "<Esc>", "Leave Insert mode" },
   },
   n = {
--- create new tab
-    ["te"] = {":tabnew<Return>", "new tab"},
+    -- create new tab
+    ["te"] = { ":tabnew<Return>", "new tab" },
     -- move beginning of line or ending of line
-    ["<leader>h"] = {"^", "move beginning of line"},
-    ["<leader>l"] = {"$", "move beginning of line"},
+    ["<leader>h"] = { "^", "move beginning of line" },
+    ["<leader>l"] = { "$", "move beginning of line" },
 
     -- Split window
-    ["ss"] = {":split<Return><C-w>w","split window vertically"},
-    ["sv"] = {":vsplit<Return><C-w>w","split window horizontally"},
+    ["ss"] = { ":split<Return><C-w>w", "split window vertically" },
+    ["sv"] = { ":vsplit<Return><C-w>w", "split window horizontally" },
 
     -- forced termination
-    ["<leader>q"] = {":<C-u>q!<Return>", "forced termination"},
+    ["<leader>q"] = { ":<C-u>q!<Return>", "forced termination" },
+
+    --  show error
+    ["qp"] = { "<Cmd>lua vim.diagnostic.setqflist()<CR>" }
   }
-    
+
 }
 
 return M
