f681498dfa3077af4d63503be071cf5fbeb788f2
Mizuki <mbaba@kanolab.net>
2024-04-23 14:16:14 +0900

    fix: nvdashが利用できるよう設定

--- dotfiles/nvchard_custom/chadrc.lua
+++ dotfiles/nvchard_custom/chadrc.lua
@@ -1,6 +1,22 @@
 ---@type ChadrcConfig
 local M = {}
-M.ui = { theme = 'everblush', transparency = true }
+local header = require("custom.headers")
+M.ui = {
+  theme = 'github_dark',
+  transparency = true,
+  nvdash = {
+    load_on_startup = true,
+    -- nvdash (dashboard)
+    header = {
+      [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+      [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+      [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+      [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+      [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+      [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+    }
+  }
+}
 M.mappings = require "custom.mappings"
 M.plugins = "custom.plugins"
 
