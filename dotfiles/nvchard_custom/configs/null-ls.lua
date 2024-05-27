12825025e626207d184a87f2a05fc55f9edb74d2
Mizuki <mbaba@kanolab.net>
2024-05-27 10:33:55 +0900

    fix: prettierを正しく利用できるようなsettingに変更

--- dotfiles/nvchard_custom/configs/null-ls.lua
+++ dotfiles/nvchard_custom/configs/null-ls.lua
@@ -4,7 +4,7 @@ require('null-ls').setup({
     null_ls.builtins.diagnostics.eslint.with({
       prefer_local = "node_modules/.bin", --プロジェクトローカルがある場合はそれを利用
     }),
-    null_ls.builtins.formatting.prettierd,
+    null_ls.builtins.formatting.prettier,
     null_ls.builtins.formatting.black, -- python formatter
 		null_ls.builtins.formatting.isort, -- python import sort
 		null_ls.builtins.diagnostics.flake8, -- python linter
